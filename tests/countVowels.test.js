8b47e109068edf512fa175a275542f1cdcfc921e
Corbin-Petersen <corbincpetersen@gmail.com>
2025-04-29 15:08:02 -0500

    refactored testing parameters

--- tests/countVowels.test.js
+++ tests/countVowels.test.js
@@ -1,7 +1,16 @@
 const countVowels = require('../countVowels');
 
 describe("countVowels", function() {
-    test("Must take a string and return the number of vowels.", () => {
+    test("Must take a number and return 0.", () => {
         expect(countVowels(674773)).toBe(0);
+    });
+    test("Must take a string and return number of vowels", () => {
+        expect(countVowels("hello")).toBe(2);
+    });
+    test("Must take an empty string and return number of vowels", () => {
+        expect(countVowels("")).toBe(0);
+    });
+    test("Must take a string and return number of vowels", () => {
+        expect(countVowels("this is a string")).toBe(4);
     });
  });
